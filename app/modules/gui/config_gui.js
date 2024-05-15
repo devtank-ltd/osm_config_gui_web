5c78c29e788d82bd12652c78073d87f0243c1179
Marcus Holder <marcus.holder@devtank.co.uk>
2024-05-15 10:54:36 +0100

    Wait for port to open creating serial object.

--- app/modules/gui/config_gui.js
+++ app/modules/gui/config_gui.js
@@ -10,7 +10,7 @@ class config_gui_t {
         this.add_event_listeners();
     }
 
-    async open_serial() {
+    open_serial() {
         const filter = { usbVendorId: '0x10c4' };
         const type = 'Serial';
         navigator.serial
@@ -19,47 +19,53 @@ class config_gui_t {
                 this.port = port;
                 this.port.getInfo();
 
-                this.port.open({
-                    baudRate: 115200, databits: 8, stopbits: 1, parity: 'none',
-                });
-
                 navigator.serial.addEventListener('connect', () => {
 
                 });
 
                 this.port.addEventListener('disconnect', () => {
                     this.port.close();
-                    this.disconnect_modal();
+                    this.disconnect_modal('OSM disconnected, return to home page');
                 });
-                const loader = document.getElementById('loader');
-                loader.style.display = 'block';
-                this.dev = new binding_t(this.port, type);
-                this.writer = await this.dev.open_ll_obj();
-                if (this.writer) {
-                    this.home = new home_tab_t(this.dev, false);
-                    await this.home.create_navbar();
-                    await this.home.insert_homepage();
-                    const disconnect = document.getElementById('global-disconnect');
-                    disconnect.addEventListener('click', () => {
-                        this.port.close();
-                        this.port = null;
-                        window.location.reload();
+                this.port.open({
+                    baudRate: 115200, databits: 8, stopbits: 1, parity: 'none',
+                })
+                    .then(async () => {
+                        const loader = document.getElementById('loader');
+                        loader.style.display = 'block';
+                        this.dev = new binding_t(this.port, type);
+                        this.writer = await this.dev.open_ll_obj();
+                        if (this.writer) {
+                            this.home = new home_tab_t(this.dev, false);
+                            await this.home.create_navbar();
+                            await this.home.insert_homepage();
+                            const disconnect = document.getElementById('global-disconnect');
+                            disconnect.addEventListener('click', async () => {
+                                loader.style.display = 'block';
+                                await this.port.close();
+                                await this.port.forget();
+                                this.port = null;
+                                window.location.reload();
+                            });
+                            const globalbtns = document.getElementById('global-load-save-config-buttons');
+                            globalbtns.style.removeProperty('display');
+                        } else {
+                            const error_div = document.getElementById('error-div');
+                            error_div.textContent = 'Failed to connect, refresh page and try again.';
+                        }
                     });
-                    const globalbtns = document.getElementById('global-load-save-config-buttons');
-                    globalbtns.style.removeProperty('display');
-                } else {
-                    const error_div = document.getElementById('error-div');
-                    error_div.textContent = 'Failed to connect, refresh page and try again.';
-                }
-            })
+                    })
             .catch((e) => {
+                this.disconnect_modal('Could not connect to OSM');
                 console.log(e);
             });
     }
 
-    async disconnect_modal() {
+    async disconnect_modal(text = 'Disconnected') {
         this.dialog = document.getElementById('osm-disconnect-dialog');
+        this.dialog_label = document.getElementById('osm-disconnect-dialog-label');
         this.confirm = document.getElementById('osm-disconnect-confirm');
+        this.dialog_label.textContent = text;
         this.dialog.showModal();
         const controller = new AbortController();
 
@@ -100,6 +106,7 @@ class config_gui_t {
             disconnect.addEventListener('click', async () => {
                 await this.dev.ll.write('close');
                 await this.dev.ll.url.close();
+                window.location.reload();
             });
 
             const globalbtns = document.getElementById('global-load-save-config-buttons');
